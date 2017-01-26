448e2ea371c0552a6996468cd2455db802f26ad1
Spencer Tweedy <spencer@spencertweedy.com>
2017-01-26 15:18:01 -0600

    Major WP overhauling

--- wp/footer.php
+++ wp/footer.php
@@ -13,35 +13,19 @@
  */
 
 ?>
+		</main>
+		
+		<footer>
+			<h2>Broadcasting from Lawrence University since 1956. <br />Home of the Great Midwest Trivia Contest.</h2>
 
-		</div><!-- #content -->
-
-		<footer id="colophon" class="site-footer" role="contentinfo">
-			<div class="wrap">
-				<?php
-				get_template_part( 'template-parts/footer/footer', 'widgets' );
-
-				if ( has_nav_menu( 'social' ) ) : ?>
-					<nav class="social-navigation" role="navigation" aria-label="<?php _e( 'Footer Social Links Menu', 'twentyseventeen' ); ?>">
-						<?php
-							wp_nav_menu( array(
-								'theme_location' => 'social',
-								'menu_class'     => 'social-links-menu',
-								'depth'          => 1,
-								'link_before'    => '<span class="screen-reader-text">',
-								'link_after'     => '</span>' . twentyseventeen_get_svg( array( 'icon' => 'chain' ) ),
-							) );
-						?>
-					</nav><!-- .social-navigation -->
-				<?php endif;
-
-				get_template_part( 'template-parts/footer/site', 'info' );
-				?>
-			</div><!-- .wrap -->
+			<nav>
+				<a href="#">About</a>
+	            <a href="#">News</a>
+	            <a href="#">Trivia</a>
+	            <a href="#">Contact</a>
+			</nav>
 		</footer><!-- #colophon -->
-	</div><!-- .site-content-contain -->
-</div><!-- #page -->
 <?php wp_footer(); ?>
-
+	</div><!-- #wrapper -->
 </body>
 </html>
